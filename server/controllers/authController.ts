08acf3ad8f1c1d90d5d6e10f1986c776d8dd4d6b
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-09-24 11:39:45 +0530

    feat: implement user registration with email validation and password hashing

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,6 +1,75 @@
-import { Request } from "express";
+import { Request, Response } from "express";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import isValidEmail from "../utils/validateEmail.js";
+import { prisma } from "../config/prisma.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
 
+const adminStatus = (email: string | null | undefined): boolean => {
+  if (!email) return false;
 
-export const registerUser = (req: Request, res: Response) => {
+  const adminEmails = process.env.ADMIN_EMAILS
+    ? process.env.ADMIN_EMAILS.split(",").map((e) =>
+        e.trim().toLocaleLowerCase(),
+      )
+    : [];
 
-}
+  return adminEmails.includes(email.toLocaleLowerCase().trim());
+};
+
+export const registerUser = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+      throw new ApiError(400, "Please provide all fields");
+    }
+
+    const normalizedEmail = email.trim().toLowerCase();
+
+    if (!isValidEmail(normalizedEmail)) {
+      throw new ApiError(400, "Invalid email format");
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: normalizedEmail,
+      },
+    });
+
+    if (existingUser) {
+      throw new ApiError(400, "User already exists with this email");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email: normalizedEmail,
+        password: hashedPassword,
+        name,
+      },
+    });
+
+    const token = generateToken(user.id);
+
+    const { password: _, ...userData } = user;
+    const userResponse = {
+      ...userData,
+      isAdmin: adminStatus(userData.email),
+    };
+
+    res.status(201).json(
+      new ApiResponse(
+        201,
+        {
+          user: userResponse,
+          token,
+        },
+        "User successfully registered",
+      ),
+    );
+  },
+);
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,16 @@
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import authRouter from "./routes/authRoute.js";
 
 const app = express();
 
 // Middleware
 app.use(cors())
 app.use(express.json());
+
+// Routes
+app.use('/api/user', authRouter)
 
 const port = process.env.PORT || 5000;
 
